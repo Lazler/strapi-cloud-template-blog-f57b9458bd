74c76ba3a2efd980305d97b1e6ec09bd73848792
Lazler <laszlo.m.g@gmail.com>
2025-12-09 21:38:09 +0100

    Refactor CORS middleware configuration for improved clarity and customization

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,18 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      // Ersetzen Sie dies durch Ihre erlaubten Origins
+      origin: ['http://localhost:3000', 'https://getbounz.com'],
+      // Fügen Sie alle notwendigen Methoden hinzu
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      // Fügen Sie alle Header hinzu, die Sie senden
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
